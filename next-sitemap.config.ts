7e8a3eb776811d4a36fc2ad6ffef0927d1f2440e
MikeTKD-91 <mikerusso91@gmail.com>
2026-03-01 19:42:14 +0100

    Create next-sitemap.config.ts

---
+++ next-sitemap.config.ts
@@ -0,0 +1,8 @@
+import type { IConfig } from 'next-sitemap';
+const config: IConfig = {
+  siteUrl: 'https://ristrutturazionepreventivi.it',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.8
+};
+export default config;
