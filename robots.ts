976e8eec24f94bbc5435a48271848acd6f8dfe03
MikeTKD-91 <mikerusso91@gmail.com>
2026-03-01 21:16:23 +0100

    Create robots.ts

---
+++ robots.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from 'next'
+
+const baseUrl = 'https://ristrutturazionepreventivi.it'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      // disallow: ['/api/'] // se vuoi escludere qualcosa
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  }
+}
