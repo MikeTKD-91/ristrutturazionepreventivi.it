22826983e05a5fbe9d8eb0a2b1aaab8dd048081e
MikeTKD-91 <mikerusso91@gmail.com>
2026-03-16 23:34:25 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   // ⚠️ NOTA: con output:"export" i redirect qui NON vengono applicati.
   // I redirect 301 per /napoli/ e /caserta/ vanno gestiti nel file vercel.json
 };
